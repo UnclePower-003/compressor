dc58ee4d2afad7aea86ed94e53681f9affb93557
Aaryan D. Dangol <aaryandangol.m@gmail.com>
2026-03-07 13:57:16 +0545

    Added code

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,6 @@ export default defineConfig({
   server: {
     host: "0.0.0.0",
     port: 5173,
-    allowedHosts: ["unclepowercompressor.infinitecoretechcloud.com"],
+    allowedHosts: ["unclepowercompressor.infinitecoretechcloud.com","infinitecoretechcloud.com"],
   },
 });
